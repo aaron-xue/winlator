e9d9083a286fb300e3c25774dcb982daefe4026c
xch1523480 <xch1523480@qq.com>
2026-03-12 13:15:30 +0800

    commit

--- app/src/main/java/com/winlator/cmod/core/DefaultVersion.java
+++ app/src/main/java/com/winlator/cmod/core/DefaultVersion.java
@@ -5,7 +5,7 @@ public abstract class DefaultVersion {
     public static final String WOWBOX64 = "0.4.1";
     public static final String FEXCORE = "2603";
     public static final String WRAPPER = "System";
-    public static final String WRAPPER_ADRENO = "turnip_Jan-22-2026_8g2_patched";
+    public static final String WRAPPER_ADRENO = "turnip_Mar-12-2026_b97a1e6_patched";
     public static final String DXVK = GPUInformation.getRenderer(null, null).contains("Mali") ? "1.10.3" : "2.6.2-arm64ec-gplasync";
     public static final String VKD3D = "proton-3.0b";
 }
